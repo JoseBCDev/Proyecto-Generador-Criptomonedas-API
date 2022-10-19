ab8c27cef4670981257390a9b86c9dbdaab93acd
xdgonex <bendezucortezjoseluis@gmail.com>
2022-10-18 22:09:00 -0500

    Se valido el formulario, creo objeto de busqueda y se muestra en el HTML

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 
+//Variables 
 const criptomonedasSelect = document.querySelector('#criptomonedas');
 
 const moneda = document.querySelector('#moneda');
+
+const formulario = document.querySelector('#formulario');
+
+const resultadoDiv = document.querySelector('#resultado');
 
 //Creamos una Promise para peticion de Tipos de Criptomonedas en USD al servidor, mientras lo demas se sigue ejecutando
 
@@ -10,12 +15,46 @@ const obtenerCriptomonedas = criptomonedas => new Promise(resolve =>{
 });
 
 
-
+//Cargar el documento JS 
 document.addEventListener('DOMContentLoaded',()=>{
 
     consultarCriptomonedas();
+
+    formulario.addEventListener('submit',validarFormulario);
+
+    criptomonedasSelect.addEventListener('change',leerValor);
+    moneda.addEventListener('change',leerValor);
+
 });
 
+//Objeto Busqueda
+const objBusqueda = {
+    criptomoneda: '',
+    moneda: ''
+}
+//Llenamos el objeto cada vez que seleccionamos un select
+function leerValor(e)
+{
+   objBusqueda[e.target.name] = e.target.value;
+
+    console.log(objBusqueda);
+}
+
+function validarFormulario(e)
+{
+    e.preventDefault();
+
+    const {criptomoneda,moneda} = objBusqueda;
+    //Validando que los campos esten llenos
+    if(criptomoneda==='' || moneda ==='')
+    {
+        mostrarAlerta('Todos los campos son obligatorios');
+        return;
+    }
+
+
+}
+//Extraendo informacion de la API para los tipos de Criptomonedas
 function consultarCriptomonedas(){
 
     const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD`;
@@ -25,7 +64,7 @@ function consultarCriptomonedas(){
         .then(resultado => obtenerCriptomonedas(resultado.Data))
         .then(criptomonedas => selectCriptomonedas(criptomonedas))
 }
-
+//Mostramos los tipos de criptomonedas
 function selectCriptomonedas(criptomonedas)
 {
 
@@ -38,4 +77,25 @@ function selectCriptomonedas(criptomonedas)
 
         criptomonedasSelect.appendChild(option);
     });
+}
+//Alerta cada vez que salga un fallo
+function mostrarAlerta(mensaje)
+{
+    const existeAlerta = document.querySelector('.error');
+
+    if(!existeAlerta)
+    {
+    const alerta = document.createElement('div');
+
+    alerta.classList.add('error');
+    alerta.textContent = mensaje;
+
+    resultadoDiv.appendChild(alerta);
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
+    }
+
+    
 }
